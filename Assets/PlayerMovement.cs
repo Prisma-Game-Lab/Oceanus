d4c947f3df9fd1eff9c16710b608d768aed0c9bd
nikopaes <nickxp51@hotmail.com>
2016-08-27 18:31:30 -0300

    Commit Base
    Player
    -Movement 
    -Shooting

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
+    //Variables
+    public float speed = 10f;
+    private Rigidbody2D _rigidBody;
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+
+    }
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +18,32 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+	{
+	    float x, y;
+	    x = Input.GetAxisRaw("Horizontal");
+	    y = Input.GetAxisRaw("Vertical");
+
+        moveFunction(x, y);
+        turningFunction();
 	}
+
+    void moveFunction(float x, float y)
+    {
+        Vector3 movement = new Vector3(x, y, 0f);
+        movement = movement.normalized;
+        movement *= speed * Time.deltaTime;
+        _rigidBody.MovePosition(transform.position + movement);
+    }
+
+    void turningFunction()
+    {
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse = new Vector3(mouse.x,mouse.y,0f);
+        float angle;
+        Vector3 dist = mouse - transform.position;
+        Debug.Log(dist);
+        angle = Mathf.Atan2(dist.y, dist.x);
+        _rigidBody.MoveRotation(angle * Mathf.Rad2Deg);
+    }
 }
--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -1,7 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerShooting : MonoBehaviour {
+
+	public float rateOfFire;
+    public float bulletVelocity;
+	bool _canShoot = true;
+    public GameObject bullet;
+
+    public GameObject [] BubbleBullets;
+
+    void Shoot()
+
+    {
+        Rigidbody2D rigid = ((GameObject)Instantiate(bullet, transform.position, transform.rotation)).GetComponent<Rigidbody2D>();
+        rigid.velocity = (Vector2)transform.right.normalized * bulletVelocity;
+    }
+
+    void SetShootOn ()
+    {
+            _canShoot = true;
+        
+    }
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +29,15 @@ public class PlayerShooting : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+        
+         if (_canShoot == true && Input.GetMouseButton(0))
+        {
+            Shoot();
+            _canShoot = false;
+            Invoke("SetShootOn", 1 / rateOfFire);
+        }
 	
 	}
 }
